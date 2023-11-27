d1d5bcd82385a86a5e7492fc935b40f6c37d552a
Angelo <angelo.knto@gmail.com>
2023-11-27 18:04:31 -0300

    created Place model and implemented Get method in controller

--- NewPlaces_PlacesAPI/Controllers/PlacesAPIController.cs
+++ NewPlaces_PlacesAPI/Controllers/PlacesAPIController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NewPlaces_PlacesAPI.Models;
 
 namespace NewPlaces_PlacesAPI.Controllers
 {
@@ -7,5 +8,14 @@ namespace NewPlaces_PlacesAPI.Controllers
     [ApiController]
     public class PlacesAPIController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Place> GetPlaces()
+        {
+            return new List<Place>
+            {
+                new Place { Id = 1, Name = "Sun Villa" },
+                new Place { Id = 2, Name = "Beach View" }
+            };
+        }
     }
 }
---
+++ NewPlaces_PlacesAPI/Models/Place.cs
@@ -0,0 +1,8 @@
+﻿namespace NewPlaces_PlacesAPI.Models
+{
+    public class Place
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
