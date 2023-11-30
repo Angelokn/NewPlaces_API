aaaf4187023a9d38067b53035ee490a8fc0ccf0b
Angelo <angelo.knto@gmail.com>
2023-11-29 23:02:17 -0300

    created Post method in controller

--- NewPlaces_PlacesAPI/Controllers/PlacesAPIController.cs
+++ NewPlaces_PlacesAPI/Controllers/PlacesAPIController.cs
@@ -28,7 +28,7 @@ namespace NewPlaces_PlacesAPI.Controllers
                 return BadRequest();
             }
 
-            var place = Ok(PlaceStore.placeList.FirstOrDefault(u=>u.Id==id));
+            var place = PlaceStore.placeList.FirstOrDefault(u=>u.Id==id);
 
             if (place == null)
             {
@@ -36,6 +36,28 @@ namespace NewPlaces_PlacesAPI.Controllers
             }
 
             return Ok(place);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<PlaceDTO> CreatePlace([FromBody]PlaceDTO placeDTO)
+        {
+            if (placeDTO == null)
+            {
+                return BadRequest(placeDTO);
+            }
+            if (placeDTO.Id == 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            //considering the user will always enter a distinct Id (for now):
+            placeDTO.Id = PlaceStore.placeList.OrderByDescending(u=>u.Id).FirstOrDefault().Id + 1;
+            PlaceStore.placeList.Add(placeDTO);
+
+            return Ok(placeDTO);
         }
     }
 }
