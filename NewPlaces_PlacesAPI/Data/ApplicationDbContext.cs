9b710755ffc097565dff249f83580e340d4ac216
Angelo <angelo.knto@gmail.com>
2023-12-04 18:43:33 -0300

    created ApplicationDbContext inside Data folder

---
+++ NewPlaces_PlacesAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewPlaces_PlacesAPI.Models;
+
+namespace NewPlaces_PlacesAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Place> Places { get; set; }
+    }
+}
--- NewPlaces_PlacesAPI/Models/Place.cs
+++ NewPlaces_PlacesAPI/Models/Place.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NewPlaces_PlacesAPI.Models
 {
     public class Place
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Details { get; set; }
