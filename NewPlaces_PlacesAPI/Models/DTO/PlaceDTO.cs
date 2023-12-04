c8dfd205e6d18fa432fa04fdda6cbef9134ebfac
Angelo <angelo.knto@gmail.com>
2023-12-04 18:27:38 -0300

    updated Place and PlaceDTO models

--- NewPlaces_PlacesAPI/Models/DTO/PlaceDTO.cs
+++ NewPlaces_PlacesAPI/Models/DTO/PlaceDTO.cs
@@ -8,7 +8,14 @@ namespace NewPlaces_PlacesAPI.Models.Dto
         [Required]
         [MaxLength(30)]
         public string Name { get; set; }
+        [Required]
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        [Required]
         public int Occupancy { get; set; }
+        [Required]
         public int Sqft { get; set; }
+        public string ImageUrl { get; set; }
+
     }
 }
--- NewPlaces_PlacesAPI/Models/Place.cs
+++ NewPlaces_PlacesAPI/Models/Place.cs
@@ -1,9 +1,18 @@
-﻿namespace NewPlaces_PlacesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewPlaces_PlacesAPI.Models
 {
     public class Place
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        public int Occupancy { get; set; }
+        public int Sqft { get; set; }
+        public string ImageUrl { get; set; }
         public DateTime CreationDate { get; set; }
+        public DateTime UpdateDate { get; set; }
     }
 }
